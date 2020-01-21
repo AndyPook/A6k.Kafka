689fe147e0b457df59576ec0c481a37a4aba039a
Andy Pook <andy.pook@gmail.com>
2020-01-21 14:23:10 +0000

    demo consoles

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,90 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Confluent.Kafka;
 
 namespace ConsoleApp1
 {
+    // uses Confluent.Kafka to generate some traffix to analyse with WireShark
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                Console.WriteLine("cancelled...");
+                cts.Cancel();
+                e.Cancel = true;
+            };
+
+            //DoAdmin();
+
+            _ = Consume(cts.Token);
+
+            Console.WriteLine("Produce -----");
+
+            //Produce();
+
+            Console.WriteLine("done...");
+            Console.ReadLine();
+        }
+
+        private static void Produce()
+        {
+            var producer = new ProducerBuilder<string, string>(
+                new AdminClientConfig()
+                {
+                    BootstrapServers = "localhost:29092"
+                })
+                .Build();
+
+            producer.Produce("test-topic", new Message<string, string> { Key = "fred", Value = "flintstone" });
+            producer.Produce("test-topic", new Message<string, string> { Key = "wilma", Value = "flintstone" });
+
+            var h = new Headers();
+            h.Add("age", new byte[] { 0 });
+            producer.Produce("test-topic", new Message<string, string> { Key = "bambam", Value = "flintstone", Headers = h });
+        }
+
+        private static async Task Consume(CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+            Console.WriteLine("consuming...");
+            var consumer = new ConsumerBuilder<string, string>(
+                    new ConsumerConfig
+                    {
+                        BootstrapServers = "localhost:29092",
+                        GroupId = "testgroup-" + Guid.NewGuid().ToString(),
+                        EnablePartitionEof = true
+                    }
+                )
+                .SetPartitionsAssignedHandler((_, p) =>p.Select(tp => new TopicPartitionOffset(tp, Offset.Beginning)).ToList())
+                .Build();
+
+            consumer.Subscribe("test-topic");
+
+            var timeout = TimeSpan.FromMilliseconds(200);
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var msg = consumer.Consume(timeout);
+                if (msg == null)
+                    continue;
+                if (msg.IsPartitionEOF)
+                    break;
+
+                Console.WriteLine($"key={msg.Key} value={msg.Value}");
+            }
+
+            consumer.Dispose();
+            Console.WriteLine("...consumer stopped");
+        }
+
+        private static void DoAdmin()
+        {
+            Console.WriteLine("Admin -----");
 
             var admin = new AdminClientBuilder(
                 new AdminClientConfig()
@@ -19,11 +96,8 @@ namespace ConsoleApp1
 
             var metadata = admin.GetMetadata(TimeSpan.FromSeconds(5));
             Console.WriteLine($"{metadata.OriginatingBrokerId} - {metadata.OriginatingBrokerName}");
-            foreach(var topic in metadata.Topics)
+            foreach (var topic in metadata.Topics)
                 Console.WriteLine($"  {topic.Topic}");
-
-            Console.WriteLine("done...");
-            Console.ReadLine();
         }
     }
 }
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 
 namespace TestConsole
 {
+    // simple console app to do *basic* tests/confirmations against a single broker Kafka instance 
+
     class Program
     {
         static async Task Main(string[] args)
@@ -27,15 +29,78 @@ namespace TestConsole
                 .UseConnectionLogging()
                 .Build();
 
+            Console.WriteLine(BitConverter.IsLittleEndian?"little":"big");
+            
             await using var connection = await client.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 29092));
             var kafka = new KafkaProtocol(connection, "fred");
             Console.WriteLine("\n\n");
-            
-            await GetVersion(kafka);
-            await GetMetadata(kafka);
+
+            //await GetVersion(kafka);
+            //await GetMetadata(kafka);
+            //await Produce(kafka);
+            await FindCoordinator(kafka);
 
             Console.WriteLine("done...");
             Console.ReadLine();
+        }
+
+        private static async Task FindCoordinator(KafkaProtocol kafka)
+        {
+            Console.WriteLine("----- FindCoordinator");
+
+            var response = await kafka.FindCoordinator(Guid.NewGuid().ToString());
+            Console.WriteLine($"throttle:     {response.ThrottleTime}");
+            Console.WriteLine($"errorCode:    {response.ErrorCode}");
+            Console.WriteLine($"errorMessage: {response.ErrorMessage}");
+            Console.WriteLine($"nodeId:       {response.NodeId}");
+            Console.WriteLine($"host:         {response.Host}");
+            Console.WriteLine($"port:         {response.Port}");
+        }
+        private static async Task JoinGroup(KafkaProtocol kafka)
+        {
+            Console.WriteLine("----- JoinGroup");
+
+            var response = await kafka.JoingGroup(new JoinGroupRequest
+            {
+                GroupId = "test_"+Guid.NewGuid().ToString(),
+                SessionTimeout =10000,
+                RebalanceTimeout = 300000,
+                ProtocolType = "consumer",
+                Protocols = new JoinGroupRequest.Protocol[]
+                {
+                    new JoinGroupRequest.Protocol{ Name= "range", }
+                }
+            });
+
+            Console.WriteLine($"throttle:     {response.ThrottleTime}");
+            Console.WriteLine($"errorCode:    {response.ErrorCode}");
+            Console.WriteLine($"genId:        {response.GenerationId}");
+            Console.WriteLine($"ProtocolName: {response.ProtocolName}");
+            Console.WriteLine($"Leader:       {response.Leader}");
+            Console.WriteLine($"memberId:     {response.MemberId}");
+            foreach(var m in response.Members)
+                Console.WriteLine($"  memberId:{m.MemberId} group:{m.GroupInstanceId} meta:{(m.Metadata?.Length.ToString() ?? "NONE")}");
+        }
+
+        private static async Task Produce(KafkaProtocol kafka)
+        {
+            Console.WriteLine("----- Produce");
+
+            var msg = new Message<string, string>
+            {
+                Key = "fred",
+                Value = "flintstone " + Guid.NewGuid().ToString()
+            };
+            Console.WriteLine(msg.Value);
+
+            var response = await kafka.Produce("test-topic", msg, IntrinsicWriter.String, IntrinsicWriter.String);
+            Console.WriteLine($"throttle: {response.ThrottleTime}");
+            foreach (var r in response.Responses)
+            {
+                Console.WriteLine($"topic: {r.Topic}");
+                foreach (var p in r.Partitions)
+                    Console.WriteLine($"    {p.Partition} (error@ {p.ErrorCode}) - {p.BaseOffset} {p.LogStartOffset} {Timestamp.UnixTimestampMsToDateTime(p.LogAppendTime)}");
+            }
         }
 
         private static async Task GetVersion(KafkaProtocol kafka)
