e2693cd676db46bb493561c469a26f9bbf472c32
Andy Pook <andy.pook@gmail.com>
2020-01-31 17:39:21 +0000

    simple Kafka exception class

---
+++ A6k.Kafka/KafkaException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace A6k.Kafka
+{
+    public class KafkaException : Exception
+    {
+        public KafkaException(string message) : base(message) { }
+
+        public KafkaException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
